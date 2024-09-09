7daec34bd015af551d8e423bb9ce71612977f9b7
Tariq Alz <tariqalz13579@outlook.com>
2024-09-09 20:51:37 +0300

    Initial commit

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -17,5 +17,14 @@ public class AppTest {
         assertEquals(5, app.add(2, 3));
         assertEquals(313, app.add(25, 288));
         assertEquals(1025, app.add(347, 678));
+
+        assertEquals(1, app.sub(3,2 ));
+        assertEquals(263, app.sub(288,25 ));
+        assertEquals(331, app.sub(678,347 ));
+
+        assertEquals(6, app.multiply(2, 3));
+        assertEquals(7200, app.multiply(25, 288));
+        assertEquals(235266, app.multiply(347, 678));
     }
+
 }
